456fdc1b464f0a8dc1a111fd98ba1b0f066a451a
Kurt Niemi <kurtn718@gmail.com>
2024-12-20 10:13:18 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,10 +4,9 @@ const HomePage = () => {
     <div className='hero min-h-screen bg-base-200'>
       <div className='hero-content text-center'>
         <div className='max-w-md'>
-          <h1 className='text-6xl font-bold text-primary'>Thankful Thoughts</h1>
+          <h1 className='text-6xl font-bold text-primary'>Simple Chat</h1>
           <p className='py-6 text-lg leading-loose'>
-            Thankful Thoughts is an AI-powered app that helps you express
-            gratitude and improve your mental health.
+            Hope that you find this repo useful and make an amazing AI-powered app 
           </p>
           <Link href='/chat' className='btn btn-secondary '>
             Get Started
